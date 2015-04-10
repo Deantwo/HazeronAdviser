83c443f19e541c7403e3fc67c6445d45d638dbee
Deantwo <dean@samsig.dk>
2015-04-10 12:34:06 +0200

    AssemblyDescription
    No idea where you can actually read this, but whatever it looks nice.
    I was looking at the AssemblyVersion, but I am still not sure I wanna
    mess with it, since I already have my own version numbering in Main.cs.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("HazeronAdviser")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("3rd-party Shores of Hazeron helper application")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("HazeronAdviser")]
